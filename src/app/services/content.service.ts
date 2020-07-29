2d30789fef2a7bb6ed05950d1d43647e9b0c9a36
andrew2020wit <andrew2020wit@gmail.com>
2020-07-29 14:31:13 +0300

    ContentService Refactoring

--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 interface ILink {
-  id: string;
+  id?: string;
   name: string;
   link: string;
 }
 interface IBlockLink {
-  blockId: string;
+  blockId?: string;
   blockName: string;
   links: ILink[];
 }
@@ -13,68 +13,55 @@ interface IBlockLink {
 export class ContentService {
   public blockPages: IBlockLink[] = [
     {
-      blockId: '1b',
       blockName: 'Bases',
       links: [
-        { id: '1b1', name: 'Forms: Story', link: '/forms' },
-        { id: 'animations', name: 'Animations', link: '/animations' },
-        { id: 'flexboxes', name: 'Flexbox', link: '/flex-boxes' },
-        { id: 'grid', name: 'Grid Layout', link: '/grid' },
-        { id: 'indexeddblog', name: 'IndexedDB-Log', link: '/indexed-dblog' },
-        { id: 'ngrx1', name: 'ngrx1', link: '/ngrx1' },
-        /*         {
-          id: 'ngrxusersarticles',
-          name: 'ngrx-users-articles',
-          link: '/ngrx-users-articles',
-        }, */
+        { name: 'Forms: Story', link: '/forms' },
+        { name: 'Animations', link: '/animations' },
+        { name: 'Flexbox', link: '/flex-boxes' },
+        { name: 'Grid Layout', link: '/grid' },
+        { name: 'IndexedDB-Log', link: '/indexed-dblog' },
+        { name: 'ngrx1', link: '/ngrx1' },
       ],
     },
     {
-      blockId: '3b',
       blockName: 'RxJS',
       links: [
-        { id: '3b1', name: 'RxJS', link: '/rx/rx-intro' },
-        { id: '3b4', name: 'Http: GitHub API', link: '/rx/http' },
+        { name: 'RxJS', link: '/rx/rx-intro' },
+        { name: 'Http: GitHub API', link: '/rx/http' },
         {
-          id: 'httpjsonplaceholder',
           name: 'Http: CRUD',
           link: '/rx/http-json-placeholder',
         },
       ],
     },
     {
-      blockId: '4b',
       blockName: 'Material Design',
       links: [
-        { id: '4b2', name: 'Tables', link: '/demo-material/tables' },
+        { name: 'Tables', link: '/demo-material/tables' },
         {
-          id: '4b3',
           name: 'GuiTestAreaComponent',
           link: '/demo-material/gui-test-area',
         },
         {
-          id: 'platformcdk',
           name: 'Platform-cdk',
           link: '/demo-material/platform-cdk',
         },
       ],
     },
     {
-      blockId: '8b',
       blockName: 'TempBlock',
-      links: [{ id: '1b6', name: 'Temp-code', link: '/temp-component' }],
+      links: [{ name: 'Temp-code', link: '/temp-component' }],
     },
     {
-      blockId: '9b',
       blockName: 'Archive',
       links: [
-        { id: 'bindings', name: 'Bindings', link: '/bindings' },
-        { id: 'test-area', name: 'test-area', link: '/test-area' },
-        { id: 'parent', name: 'Parent-Child interaction', link: '/parent' },
-        { id: 'directives', name: 'Directives', link: '/directives' },
-        { id: 'pipes', name: 'Pipes', link: '/pipes' },
-        { id: 'route2', name: 'ActivatedRoute', link: '/route2' },
-        { id: 'secret', name: 'SecretPage', link: '/secret' },
+        { name: 'Bindings', link: '/bindings' },
+        { name: 'test-area', link: '/test-area' },
+        { name: 'Parent-Child interaction', link: '/parent' },
+        { name: 'Directives', link: '/directives' },
+        { name: 'Pipes', link: '/pipes' },
+        { name: 'ActivatedRoute', link: '/route2' },
+        { name: 'SecretPage', link: '/secret' },
       ],
     },
   ];
